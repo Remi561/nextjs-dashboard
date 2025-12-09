e6ac3c3a27b678ab5722b914c8c5394a1928dcec
Remy <segunismail1234@gmail.com>
2025-12-09 21:41:27 +0100

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Page = () => {
+  return (
+    <div>this is the customer page</div>
+  )
+}
+
+export default Page
---
+++ app/ui/fonts.ts
@@ -0,0 +1,6 @@
+import { Inter, Lusitana } from 'next/font/google'
+
+
+export const InterFont = Inter({ subsets: ['latin'] })
+export const LusitanaFont = Lusitana({subsets: ['latin'], weight: ["400" ,"700"] } )
+
