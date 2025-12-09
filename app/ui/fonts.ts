3fea499207b46685d49e851aba6488d2bac2ff03
Remy <segunismail1234@gmail.com>
2025-12-09 21:55:31 +0100

    bug fixed

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -2,5 +2,8 @@ import { Inter, Lusitana } from 'next/font/google'
 
 
 export const InterFont = Inter({ subsets: ['latin'] })
-export const LusitanaFont = Lusitana({subsets: ['latin'], weight: ["400" ,"700"] } )
+export const lusitana = Lusitana({
+  subsets: ["latin"],
+  weight: ["400", "700"],
+});
 
